8922788e40c805661c96e0731ca34d30c3707a52
Idoko Emmanuel <idemmanuel001@gmail.com>
2023-02-09 09:53:16 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,14 @@ import { useParameterContext } from './context/dataContext';
 import { Box, Stack, TextField, Typography } from '@mui/material';
 import ProductsTable from './components/ProductsTable';
 import Pagination from './components/Pagination';
+import { width } from '@mui/system';
 
+
+const containerStyle = {
+    height: '100%',
+    width: '100%',
+    bgColor: '#000'
+};
 
 const App: React.FC = () => {
     const { filter, setFilter } = useParameterContext();
@@ -27,27 +34,29 @@ const App: React.FC = () => {
 
 
     return (
+        <Box sx={{ background: '#000', height: '100vh' }}>
+            <div className='blueBg'></div>
+            <div className='whiteBg'></div>
 
-        <Box maxWidth="md" mx='auto' py={5} my={5} sx={{ background: '#eee' }}>
-            <Stack direction='column' spacing={4} alignItems='center' justifyContent='center' >
-                <Typography variant='h2' textAlign='center' > Products</Typography>
-                <TextField
-                    type='text'
-                    id="filter"
-                    label="filter by id"
-                    helperText="Please enter a valid product id"
-                    variant="outlined"
-                    maxRows={1}
-                    onChange={handleFilterChange}
-                    value={filter}
-                    sx={{ width: "100%", maxWidth: '255px' }}
-                />
+            <Box maxWidth="md" mx='auto' py={5} my={5} sx={{ background: '#eee' }}>
+                <Stack direction='column' spacing={4} alignItems='center' justifyContent='center' >
+                    <Typography variant='h2' textAlign='center' > Products</Typography>
+                    <TextField
+                        type='text'
+                        id="filter"
+                        label="filter by id"
+                        helperText="Please enter a valid product id"
+                        variant="outlined"
+                        maxRows={1}
+                        onChange={handleFilterChange}
+                        value={filter}
+                        sx={{ width: "100%", maxWidth: '255px' }}
+                    />
 
-                <ProductsTable />
-                <Pagination />
-            </Stack>
-
-
+                    <ProductsTable />
+                    <Pagination />
+                </Stack>
+            </Box>
         </Box>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import CssBaseline from '@mui/material/CssBaseline';
+import GlobalStyles from '@mui/material/GlobalStyles';
 import ContextProvider from './context/dataContext';
 
 
+const myGlobalStyle = <GlobalStyles styles={{
+  html: {
+    height: '100vh',
+    width: '100%'
+  },
+  body: {
+    height: '100vh',
+    width: '100%',
+  }
+}} />;
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,6 +23,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <CssBaseline />
+    {myGlobalStyle}
     <ContextProvider>
       <App />
     </ContextProvider>
